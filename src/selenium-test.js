22e30b201b4d732de51eb73f281e2fe6fc342572
Vlad Orlenko <vlad@orlenko.ca>
2022-10-04 15:33:22 -0700

    Remote

--- src/selenium-test.js
+++ src/selenium-test.js
@@ -1,13 +1,18 @@
-const { Builder } = require('selenium-webdriver');
-// require("chromedriver");
+const { Builder, Capabilities } = require("selenium-webdriver");
+var capabilities = Capabilities.chrome();
 
 (async function helloSelenium() {
-  let driver = await new Builder().forBrowser('chrome').build();
-
-  await driver.get('https://foaas.com/cool/RRR2D2');
-
-  const title = await driver.getTitle();
-  console.log(`Got the title of the page, ${title}`);
-
-  await driver.quit();
+  let driver = new Builder()
+        .usingServer("http://localhost:4444")
+        .withCapabilities(capabilities)
+        .build();
+    try {
+        await driver.get('https://foaas.com/cool/RRR2D2');
+        const title = await driver.getTitle();
+        console.log(`Got the title of the page, ${title}`);
+    } catch(e) {
+      console.error('Unexpected!', e);
+    } finally {
+        await driver.quit();
+    }
 })();
