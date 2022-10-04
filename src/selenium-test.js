219198b8b883ebb7ef0e294e7762aef430ddfbbe
Vlad Orlenko <vlad@orlenko.ca>
2022-10-04 15:20:47 -0700

    deps

--- src/selenium-test.js
+++ src/selenium-test.js
@@ -1,7 +1,13 @@
+const {Builder} = require('selenium-webdriver');
+require("chromedriver");
 
-(async function () {
-  driver = await new Builder().forBrowser('chrome').build();
-  await driver.get('https://foaas.com/cool/RRR2D2');
-  let title = await driver.getTitle();
-  console.log(`The title is ${title}`);
+(async function helloSelenium() {
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    await driver.get('https://foaas.com/cool/RRR2D2');
+
+    const title = await driver.getTitle();
+    console.log(`Got the title of the page, ${title}`);
+
+    await driver.quit();
 })();
