3acab6fd1f69d8bf1a4e142526f265a5feefdc1e
Vlad Orlenko <vlad@orlenko.ca>
2022-10-04 13:37:58 -0700

    update js

--- src/selenium-test.js
+++ src/selenium-test.js
@@ -1,11 +1,4 @@
-const puppeteer = require('puppeteer');
-
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://foaas.com/cool/RRR2D2');
-  const h1 = await page.$('h1');
-  console.log(`Got the page, and its header says, ${await h1.getProperty('innerHTML')}`);
-
-  await browser.close();
-})();
+driver = await new Builder().forBrowser('chrome').build();
+await driver.get('https://foaas.com/cool/RRR2D2');
+let title = await driver.getTitle();
+console.log(`The title is ${title}`);
