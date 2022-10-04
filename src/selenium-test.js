5604f51df1544691e17ac5c061a6989a9dbf343f
Vlad Orlenko <vlad@orlenko.ca>
2022-10-04 14:22:43 -0700

    top level

--- src/selenium-test.js
+++ src/selenium-test.js
@@ -1,4 +1,7 @@
-driver = await new Builder().forBrowser('chrome').build();
-await driver.get('https://foaas.com/cool/RRR2D2');
-let title = await driver.getTitle();
-console.log(`The title is ${title}`);
+
+(async function () {
+  driver = await new Builder().forBrowser('chrome').build();
+  await driver.get('https://foaas.com/cool/RRR2D2');
+  let title = await driver.getTitle();
+  console.log(`The title is ${title}`);
+})();
