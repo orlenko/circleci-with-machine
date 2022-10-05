df9137ca7958daad83c28a6cfa670b42dbff0fea
Vlad Orlenko <vlad@orlenko.ca>
2022-10-04 18:15:07 -0700

    a bit more steps

--- src/selenium-test.js
+++ src/selenium-test.js
@@ -1,20 +1,28 @@
 const { exit } = require("process");
-const { Builder, Capabilities } = require("selenium-webdriver");
+const { Builder, Capabilities, By } = require("selenium-webdriver");
 var capabilities = Capabilities.chrome();
 
 (async function helloSelenium() {
+  const driverUrl = "http://selchrome:4444";
   let driver = new Builder()
-        .usingServer("http://selchrome:4444")
-        .withCapabilities(capabilities)
-        .build();
-    try {
-        await driver.get('https://foaas.com/cool/RRR2D2');
-        const title = await driver.getTitle();
-        console.log(`Got the title of the page, ${title}`);
-    } catch(e) {
-      console.error('Unexpected!', e);
-      exit(1);
-    } finally {
-        await driver.quit();
+    .usingServer(driverUrl)
+    .withCapabilities(capabilities)
+    .build();
+  try {
+    const url = 'https://foaas.com/cool/RRR2D2';
+    await driver.get(url);
+    const title = await driver.getTitle();
+    console.log(`Got the title of the page, ${title}`);
+    const em = await driver.findElement(By.css('p>em'));
+    const text = await em.getText();
+    const expected = '- RRR2D2';
+    if (text !== expected) {
+      throw new Error(`Expected "${text}" to equal ${expected}`);
     }
+  } catch (e) {
+    console.error('Unexpected!', e);
+    exit(1);
+  } finally {
+    await driver.quit();
+  }
 })();
